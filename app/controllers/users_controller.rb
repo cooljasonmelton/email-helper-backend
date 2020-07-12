0cbe51373d0bce0452c7dc4089842dd33ee6461e
cooljasonmelton <cooljasonmelton>
2020-07-12 01:28:12 -0500

    updated controller for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
     def index
-        render json: User.all
+        render json: User.all, include: [:templates, :contacts]
     end 
+
+    def show
+        user = User.find_by(params[:email])
+        if user
+            render json: user, include: [:templates, :contacts]
+        else
+            render json: { error: 'Invalid credentials.'}, status: 401
+        end
+    end 
+
 
 end
