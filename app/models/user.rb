285e421f881098bcce663c797768727b6c904828
cooljasonmelton <cooljasonmelton>
2020-07-13 05:04:39 -0500

    adjusted auth controller json, set some validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :templates 
     has_many :contacts
     has_many :tags, through: :contacts
+    validates :email, uniqueness: true
+    validates :email, presence: true
+    validates :password_digest, presence: true
 end
