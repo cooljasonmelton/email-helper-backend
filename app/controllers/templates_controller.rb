8fd368f586f4354ecb0cdb0f74b69d8cf38145ac
cooljasonmelton <cooljasonmelton>
2020-07-14 04:07:04 -0500

    set up template update route and connected to front end

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,14 +1,20 @@
 class TemplatesController < ApplicationController
 
     def create
-        p params
         user = User.find_by(id: params[:userId])
         user.templates << Template.create(name: params[:name], subject: params[:subject], body: params[:body])
         render json: user, include: [:templates, :contacts]
     end
 
-    private
-    def getTemplateParams
+    def update
+        user = User.find_by(id: params[:userId])
+        template = Template.find_by(id: params[:id])
+        template.update(template_params)
+        render json: user, include: [:templates, :contacts]
     end
+
+    def template_params
+        params.require(:template).permit(:name, :subject, :body)
+     end
 
 end
