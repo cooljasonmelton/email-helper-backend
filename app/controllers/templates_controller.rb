292c11f9d59f5a5584381a5640e53e09154e48d8
cooljasonmelton <cooljasonmelton>
2020-07-14 01:26:30 -0500

    user can create new templates

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,7 +1,14 @@
 class TemplatesController < ApplicationController
 
     def create
-        p 'template create, hit me!'
+        p params
+        user = User.find_by(id: params[:userId])
+        user.templates << Template.create(name: params[:name], subject: params[:subject], body: params[:body])
+        render json: user, include: [:templates, :contacts]
+    end
+
+    private
+    def getTemplateParams
     end
 
 end
