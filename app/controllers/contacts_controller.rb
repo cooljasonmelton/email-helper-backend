4e46cb9ba95a4adb042e013b50691f617bf282ae
cooljasonmelton <cooljasonmelton>
2020-07-18 02:29:50 -0500

    set up delete route for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,15 @@
 class ContactsController < ApplicationController
 
     def create
-        
         user = User.find_by(id: params[:userId])
         user.contacts << Contact.create(name: params[:name], email: params[:email])
+        render json: user, include: [:templates, :contacts]
+    end 
+
+    def destroy
+        contact = Contact.find_by(id: params[:id])
+        user = User.find_by(id: contact.user_id)
+        contact.destroy
         render json: user, include: [:templates, :contacts]
     end 
 
