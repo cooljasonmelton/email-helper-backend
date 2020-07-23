61fa8451806df3e46d9145910e2ddf14b6f9763c
cooljasonmelton <cooljasonmelton>
2020-07-23 01:12:30 -0500

    set up create user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,13 @@ class UsersController < ApplicationController
         end
     end 
 
-    def show
+    def create
+        user = User.create(email: params[:email], password_digest: params[:password])
+        if user.valid?
+            render json: user, include: [:templates, :contacts]
+        else
+            render json: { error: `#{user.errors.full_messages}`}, status: 401
+        end
 
     end 
-
-
 end
