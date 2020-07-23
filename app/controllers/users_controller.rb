89ca4204999197630c4b59ee49c7caf3cdf8a68d
cooljasonmelton <cooljasonmelton>
2020-07-23 01:24:25 -0500

    set up controllers for login, fixed accidental delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,7 @@ class UsersController < ApplicationController
     end 
 
     def login 
-        user = User.find_by(email: params[:email], password_digest: params[:password])
-        if user
-            render json: user, include: [:templates, :contacts]
-        else
-            render json: { error: 'Invalid credentials.'}, status: 401
-        end
+
     end 
 
     def create
